5f2631d17bc5f369b675796781bd04864e2cd4ca
TomaszPawłowski <Tomaszunek@users.noreply.github.com>
2020-12-29 19:18:04 +0100

    Day44 (#34)
    * day40
    
    * add day41
    
    * add day41
    
    * add day42
    
    * add day42
    
    * add day43
    
    * add day44

---
+++ src/components/Day44/DayView.vue
@@ -0,0 +1,62 @@
+<template>
+  <transition name="Day44">
+    <div>
+      <InputWrapper>
+        <span>Numbers:</span>
+        <input
+          v-model="numbersArray"
+          type="text"
+          style="width: 500px;"
+        >   
+      </InputWrapper>
+      <OutputWrapper>
+        <code> inversions count: {{ output }}
+        </code>
+      </OutputWrapper>
+    </div>
+  </transition>
+</template>
+
+<script>
+import styled from 'vue-styled-components';
+import { findInversions } from './calc';
+
+const InputWrapper = styled.section`
+  padding: 1em 4em;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const OutputWrapper = styled.section`
+  padding: 1em 4em;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+`;
+
+export default {
+  name: "Day44",
+  components: {
+    InputWrapper,
+    OutputWrapper,
+  },
+  props: {
+    numbers: {
+      type: String,
+      required: true,
+    },
+  },  
+  data() {
+    return {
+      numbersArray: this.numbers,
+    }
+  }, 
+  computed: {
+    output: function() {
+      const { numbersArray } = this;
+      const parseArray = JSON.parse(numbersArray);
+      return JSON.stringify(findInversions(parseArray));
+    }    
+  }
+};
+</script>
