e87a15afc8303826d6865ca35168136b6f3e5551
TomaszPawłowski <Tomaszunek@users.noreply.github.com>
2021-01-09 23:39:42 +0100

    Day50 (#39)
    * add day46
    
    * add
    
    * add 49
    
    * add 50

---
+++ src/components/Day50/DayView.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition name="Day50">
+    <div>
+      <OutputWrapper>
+        <code> calculation: {{ output }}
+        </code>        
+      </OutputWrapper>
+    </div>
+  </transition>
+</template>
+
+<script>
+import styled from 'vue-styled-components';
+import { calculationFromTree } from './calc';
+
+const OutputWrapper = styled.section`
+  padding: 1em 4em;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  width: 88%;
+`;
+
+export default {
+  name: "Day50",
+  components: {
+    OutputWrapper,
+  },
+  props: {
+    tree: {
+      type: Object,
+      required: true,
+    },
+  },  
+  computed: {
+    output: function() {
+      const { tree } = this;
+      console.log(tree)
+      return calculationFromTree(tree);
+    }    
+  }
+};
+</script>
